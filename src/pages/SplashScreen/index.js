c6de1e3e32ef68b087d9781c75010bd82552c8fa
Tutu_gitlab <icammultazam@gmail.com>
2021-03-01 10:38:12 +0700

    setting font family

--- src/pages/SplashScreen/index.js
+++ src/pages/SplashScreen/index.js
@@ -9,8 +9,8 @@ const SplashScreen = () => {
         <View style={{ backgroundColor:'#FFc700', flex:1,  justifyContent:"center", alignItems:"center"}} >
            <Logo/>
            <View style={{ height:38 }} />
-            <Text style={{fontSize:32, color:'#020202'}}
-                >Food Marketer</Text>
+            <Text style={{fontSize:32, color:'#020202', fontFamily:'Poppins-Medium'}}
+                >RSU Marketer</Text>
         </View>
     )
 }
