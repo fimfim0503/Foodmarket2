408cfd56078b75a654590a4533ad17e116006891
Tutu_gitlab <icammultazam@gmail.com>
2021-03-01 11:09:48 +0700

    react navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 
 import React from 'react';
+import 'react-native-gesture-handler';
 import {SafeAreaView,Text,StatusBar,} from 'react-native';
 import { SplashScreen } from './pages';
+import { NavigationContainer } from '@react-navigation/native';
+
+
 
 
 const App = () => {
   return (
-   <SplashScreen/>
+    
+    <NavigationContainer>
+      <SplashScreen/>
+    </NavigationContainer>
   );
 };
 
