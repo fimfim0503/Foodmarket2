0398458201afcca0fd3eb479011c634d78988630
Tutu_gitlab <icammultazam@gmail.com>
2021-03-03 08:45:22 +0700

    sign up address

---
+++ src/pages/SignUpAddress/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {Header, TextInput, Gap, Button, Select} from '../../components';
+
+const SingUpAddress = () => {
+    return (
+        <View style={styles.page}>
+            <Header title="Address" subTitle="Make Sure It's Valid" onBack={()=>{}} />
+           
+           
+            <View style={styles.container} >
+                
+                <TextInput label="Phone Number" placeholder="Phone Number"/>
+                <Gap height={16} />
+                <TextInput label="Address" placeholder="address"/>
+                <Gap height={16} />
+                <TextInput label="House Number" placeholder="House Number"/>
+                <Gap height={16} />
+                <Select/>
+                <Gap height={24} />
+                <Button text="Sign Up Now" />
+                
+            </View>
+        </View>
+    )
+}
+
+export default SingUpAddress
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'white',
+        paddingHorizontal:24,
+        paddingVertical:26,
+        marginTop:24,
+        flex:1
+    },
+    page:{
+        flex:1
+    },
+    
+})
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import SplashScreen from './SplashScreen';
 import SignIn from './SignIn';
 import SignUp from './Signup';
+import SignUpAddress from './SignUpAddress';
+
 
 export {
     SplashScreen,
     SignIn,
-    SignUp
+    SignUp,
+    SignUpAddress
 };
