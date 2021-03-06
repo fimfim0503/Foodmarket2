572b05803cabef74ec9968400c5dab4149770215
fimfim0503 <fimfim0503@gmail.com>
2021-03-06 15:52:22 +0700

    form local state

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Header, TextInput, Button, Gap } from '../../components'
 
 const SignIn = ({navigation}) => {
+    const [email, setEmail] =useState('');
+    const [Password, setPassword] =useState('');
+
+    const onSubmit =()=> {
+        console.log('email', email);
+        console.log('password', Password);
+    }
     return (
         <View style={styles.page} >
             <Header title="Sign in" subTitle="Find Your best evel meal" />
             <View style={styles.container} >
-                <TextInput label="Email Address" placeholder="Type your email address"/>
+                <TextInput 
+                label="Email Address" 
+                placeholder="Type your email address"
+                value={email}
+                onChangeText={(value)=>setEmail(value)}
+                
+                />
                 <Gap height={16} />
-                <TextInput label="Password" placeholder="Type Your Password"/>
+                <TextInput 
+                label="Password" 
+                placeholder="Type Your Password"
+                value={Password}
+                onChangeText={(value)=>setPassword(value)}
+                secureTextEntry
+                />
                 <Gap height={24} />
-                <Button text="Sign In" />
+                <Button text="Sign In" onPress={onSubmit} />
                 <Gap height={12} />
                 <Button  text="Create New Accunt" color='#8D92A3' textColor='white' onPress={()=> navigation.navigate('SignUp')} />
             </View>
